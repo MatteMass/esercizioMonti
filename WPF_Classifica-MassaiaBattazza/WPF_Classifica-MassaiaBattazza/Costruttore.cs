e63a3e5ac4b7bfa677068567eea792d0f3e19241
Matteo Massaia <MATTEO.MASSAIA@local.ittcesena.it>
2022-11-25 11:38:32 +0100

    aggiunta classe Costruttore

---
+++ WPF_Classifica-MassaiaBattazza/WPF_Classifica-MassaiaBattazza/Costruttore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classifica_MassaiaBattazza
+{
+    internal class Costruttore
+    {
+        internal string NomeCostruttore { get; set; }
+        internal int PunteggioCostruttore { get; set;}
+        internal int[] PilotiSelezionati { get; set; }
+
+        //costruttore 
+        public Costruttore(string nomeCostruttore, int punteggioCostruttore, int[] pilotiSelezionati)
+        {
+            NomeCostruttore = nomeCostruttore;
+            PunteggioCostruttore = punteggioCostruttore;
+            PilotiSelezionati = pilotiSelezionati;
+        }
+
+        
+
+    }
+}
